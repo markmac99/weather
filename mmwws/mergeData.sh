e296015e925ba8d28bf192127b8ec7eaabc40df3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-12-06 11:56:42 +0000

    script to merge extra data in

---
+++ mmwws/mergeData.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $here
+source $HOME/venvs/openhabstuff/bin/activate
+sudo systemctl stop getweatherdata
+outdir=$HOME/weather/raw
+tmpdir=$HOME/weather/tmp
+yr=2023
+cp $outdir/raw-$yr.parquet $outdir/bkp/raw-$yr.parquet.$(date +%Y%m%d-%h%m%s)
+python -c "from convertOHdata import mergeDataIn;mergeDataIn('$outdir', '$tmpdir', $yr);"
+sudo systemctl start getweatherdata
