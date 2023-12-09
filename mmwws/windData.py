9ae3b0f8eca8913ccaa76eea9cee995f4c354316
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-12-09 23:17:50 +0000

    adding recent wind graph

--- mmwws/windData.py
+++ mmwws/windData.py
@@ -6,6 +6,31 @@ import os
 import datetime
 
 
+def recentWind(df, outdir):
+    now=datetime.datetime.now()
+    backdt = now + datetime.timedelta(hours=-1)
+    seldf = df[df.timestamp >= pd.Timestamp(backdt, tz='UTC')]
+    outfname = os.path.join(outdir, 'dragontail-recent-wind.js')
+    with open(outfname, 'w') as of:
+        of.write("$(function() {\nMorris.Line({\n element: 'dragontail-recent-wind',\n data: [\n")
+        lastts = seldf.iloc[-1].timestamp
+        for _, rw in seldf.iterrows():
+            maxw = round(rw.wind_max_km_h * 0.6214, 1)
+            avew = round(rw.wind_avg_km_h * 0.6214, 1)
+            ts = int(rw.timestamp.timestamp()*1000)
+            of.write(f'    {{time: {ts}, ave: {avew}, gust: {maxw} }}')
+            if ts != lastts:
+                of.write(',\n')
+            else:
+                of.write('\n')
+        of.write("       ],\n        xkey: 'time',\n        ykeys: ['ave','gust'],\n")
+        of.write("       labels: ['Average','Gust'],\n        hideHover: 'auto',\n")
+        of.write("       xLabelAngle: 45,\n")
+        of.write("       postUnits: 'mph',\n        resize: true\n")
+        of.write("});\n});\n")
+    return 
+
+
 def minmaxWind(df, outdir, period=24):
     now=datetime.datetime.now()
     if period == 28*24:
@@ -55,5 +80,5 @@ def minmaxWind(df, outdir, period=24):
     if period == 24:
         outfname = os.path.join(outdir, 'dragontailrecentwind.txt')
         with open(outfname, 'w') as of:
-            of.write(f'{round(df.iloc[-1].wind_max_km_h*0.6214,1)} mph')
+            of.write(f'{round(df.wind_max_km_h.max()*0.6214,1)} mph')
     return 
