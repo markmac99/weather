a76b57be163015b0b3e91be244b8c82859079e7d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-03-09 22:55:37 +0000

    fix issue with container

--- testingcontainer/start_scaleset.sh
+++ testingcontainer/start_scaleset.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-INSTALLATION_NAME="arc-runner-ws"
+INSTALLATION_NAME="weather-runner"
 NAMESPACE="arc-runner-wh"
 GITHUB_CONFIG_URL="https://github.com/markmac99/weather"
-GITHUB_PAT="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"
+GITHUB_PAT=$(cat ~/.ssh/gh_pat_containers)
 
 helm upgrade --install "${INSTALLATION_NAME}" \
     --namespace "${NAMESPACE}" \
@@ -11,4 +11,4 @@ helm upgrade --install "${INSTALLATION_NAME}" \
     --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
     --set githubConfigSecret.github_token="${GITHUB_PAT}" \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
-
+ 
