35d2bdc2034057aaa5eea210641b393436118b39
Mark McIntyre <markmcintyre99@googlemail.com>
2026-05-29 23:10:42 +0100

    minor tweaks

--- batterymonitor/batterymonitor.ino
+++ batterymonitor/batterymonitor.ino
@@ -15,9 +15,12 @@
 #include <PubSubClient.h>
 #include "sitespecific.h"
 
+//#define LED_BUILTIN D4
+
 unsigned int raw = 0;
 float volt = 0.0;
 const int SLEEPSECS = 10;
+
 
 WiFiClient espClient;
 PubSubClient mqtt_client(espClient);
@@ -30,15 +33,15 @@ String mqtt_topic  = "sensors/batteries/";
 const int publish = 1; // set to zero to disable mq publ while testing 
 
 void connectToWiFi() {
+    WiFi.forceSleepWake();
+    delay(1000);
     WiFi.begin(ssid, password);
     Serial.print("Connecting to WiFi");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
-        digitalWrite(0, LOW); // Red on-board LED on
     }
     Serial.println("\nConnected to the WiFi network");
-    digitalWrite(0, HIGH); // Red on-board LED on
 }
 
 void connectToMQTTBroker() {
@@ -56,43 +59,53 @@ void connectToMQTTBroker() {
 }
 
 void setup() {
-    Serial.begin(115200);
-    connectToWiFi();
-    mqtt_client.setServer(mqtt_broker, mqtt_port);
-    connectToMQTTBroker();
+    Serial.begin(74880); // to ensure i can read any debug produced at boot
+    delay(100);
+    Serial.println("starting up...");
+    delay(200);
+    WiFi.forceSleepBegin();
+    pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+}
+
+void readSensors() {
+    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    raw = analogRead(A0);
+    volt = raw / 1023.0;
+    volt = volt * scalefactor;
+
+    if (publish)
+    {
+        String payload=String(volt);
+        Serial.println("attempting to send a reading");
+        connectToWiFi();
+        mqtt_client.setServer(mqtt_broker, mqtt_port);
+
+        connectToMQTTBroker();
+        String topic = mqtt_topic + topicname + "/voltage";
+        if (mqtt_client.publish(topic.c_str(), payload.c_str(), true))
+            Serial.println("Message published ["+String(topic)+"]: "+payload);
+        else
+        Serial.println("Problem publishing ["+String(topic)+"]: "+payload);
+
+        payload=String(raw);
+        topic= mqtt_topic + topicname + "/reading";
+        if (mqtt_client.publish(topic.c_str(), payload.c_str(), true))
+            Serial.println("Message published ["+String(topic)+"]: "+payload);
+        else
+        Serial.println("Problem publishing ["+String(topic)+"]: "+payload);
+    }
+    else
+    {
+        Serial.println("simulated publish");
+    }
+    Serial.println("sleeping...");
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+    uint64_t sleepTime = SLEEPSECS * 1000000;
+    ESP.deepSleep(sleepTime, WAKE_RFCAL); //, RF_NO_CAL);
+    delay(200);
 }
 
 void loop() {
-  raw = analogRead(A0);
-  volt = raw / 1023.0;
-  volt = volt * scalefactor;
-
-  if (publish)
-  {
-    String payload=String(volt);
-
-    connectToMQTTBroker();
-
-    digitalWrite(0, LOW); 
-    String topic = mqtt_topic + topicname + "/voltage";
-    if (mqtt_client.publish(topic.c_str(), payload.c_str(), true))
-        Serial.println("Message published ["+String(topic)+"]: "+payload);
-    else
-      Serial.println("Problem publishing ["+String(topic)+"]: "+payload);
-
-    payload=String(raw);
-    topic= mqtt_topic + topicname + "/reading";
-    if (mqtt_client.publish(topic.c_str(), payload.c_str(), true))
-        Serial.println("Message published ["+String(topic)+"]: "+payload);
-    else
-      Serial.println("Problem publishing ["+String(topic)+"]: "+payload);
-    digitalWrite(0, HIGH); 
-  }
-  else
-  {
-      Serial.println("simulated publish");
-  }
-  Serial.println("sleeping...");
-  ESP.deepSleep(SLEEPSECS*1e6);
-  //delay(SLEEPSECS*1000);
+    readSensors();
 }
